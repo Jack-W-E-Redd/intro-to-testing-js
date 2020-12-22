5c20f2606432482eae6a9df5fa7d28a221ad5757
jack redd <jack.w.e.redd@gmail.com>
2020-12-22 12:07:40 -0600

    added tests.js file

--- tests.js
+++ tests.js
@@ -13,3 +13,56 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+// Exercise 1:
+//friday loops
+// function putOnCoat() {
+//     let isCold = true;
+//     let counter = 0;
+//     while (isCold){
+//         console.log("I need a coat!");
+//         counter++;
+//         if(counter === 10)
+//         {
+//             isCold = false;
+//             takeOffCoat()
+//         }
+//     }
+// }
+
+
+
+// // //Create a function that makes your $100k Tesla speed until the police are looking
+
+ function speedingTesla() {
+     let keepGoing = true;
+     let counter = 0;
+     while (keepGoing) {
+         console.log("Need for speed!");
+         counter++;
+         if(counter === 100);
+         {
+             keepGoing = false;
+         }
+     }
+ }
+
+
+
+for (var i =0; i < 100; i++) {
+    if (i % 3 == 0) {
+        console.log("fizz" + i);
+    } else if (i % 5 == 0) {
+        console.log("buzz" + i);
+    } else if ((i % 3 == 0) && (i % 5 == 0));
+    {
+        console.log("fizzbuzz" );
+    }
+}
+
+
+
+
+
+
+
